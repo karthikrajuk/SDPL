2a66d138ecae3d0e4defb74e04f4b596fd3a5911
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-03-21 13:58:30 +0530

    Update request-service.component.ts
    training request

--- src/app/components/pages/request-service/request-service.component.ts
+++ src/app/components/pages/request-service/request-service.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-request-service',
@@ -21,6 +22,8 @@ export class RequestServiceComponent implements OnInit {
       phone: ['', [Validators.required]],
       device: ['', [Validators.required]]
     });
+    emailjs.init("PTmfxUAnOlAZlyhRB");
+
   }
 
   devices = ['IT-Training',
@@ -37,15 +40,28 @@ export class RequestServiceComponent implements OnInit {
   onSubmit() {
     console.log(this.contactForm.value)
     if (this.contactForm.valid) {
-      this.http.post('http://localhost:4200/contact.php', this.contactForm.value)
-        .subscribe((response: any) => {
-          this.message = response.message;
-          console.log("message", this.message);
+      this.message = 'Sending message...';
+      
+      emailjs.send("service_kuiothp", "template_xa36knk", {
+          to_name: "SDPL",
+          from_name: this.contactForm.value.name,
+          email: this.contactForm.value.email,
+          phone: this.contactForm.value.phone,
+          device: this.contactForm.value.device,
+          message: this.contactForm.value.message,
+          reply_to: this.contactForm.value.email
+      })
+      .then((response) => {
+          this.message = 'Message sent successfully!';
           this.contactForm.reset();
-        }, error => {
-          this.message = 'Error sending message. Try again later.';
-        });
-    }
+          console.log('SUCCESS!', response.status, response.text);
+      }, (error) => {
+          this.message = 'Error sending message. Please try again later.';
+          console.error('FAILED...', error);
+      });
+  } else {
+      this.message = 'Please fill in all required fields correctly.';
+  }
   }
 
 } 
