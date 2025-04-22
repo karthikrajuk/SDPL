cdb90150c7703287e4b9cd38287558b915d4ea8f
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-04-22 15:53:02 +0530

    our services

--- src/app/components/pages/our-services/our-services.component.ts
+++ src/app/components/pages/our-services/our-services.component.ts
@@ -40,6 +40,10 @@ export class OurServicesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css';
+    document.head.appendChild(link);
   }
 
   // Tabs
